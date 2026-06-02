59916155ad1b4e0e7ffdef36afd2edad7f5ff3e6
Katsuyuki Seki <skattun122@gmail.com>
2026-06-02 22:20:19 +0900

    sakuraフィルタを適用後もfxで調整できるように変更

--- sekayu_common/filter.py
+++ sekayu_common/filter.py
@@ -7,7 +7,7 @@ gi.require_version("GimpUi", "3.0")
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gimp
 
-def apply_gegl_filter(layer, operation, name, props):
+def apply_gegl_filter(layer, operation, name, props, merge=True):
     """レイヤーにGEGLフィルタを適用する関数"""
 
     f = Gimp.DrawableFilter.new(layer, operation, name)
@@ -15,6 +15,16 @@ def apply_gegl_filter(layer, operation, name, props):
     for key, value in props.items():
         config.set_property(key, value)
     layer.append_filter(f)
-    layer.merge_filters()
+    if merge:
+        layer.merge_filters()
     
     return f
+
+
+def build_gamma_curve(gamma, samples):
+    """GMAカーブ用の明るめトーンカーブを作成する関数"""
+
+    return [
+        (i / (samples - 1)) ** (1.0 / gamma)
+        for i in range(samples)
+    ]
